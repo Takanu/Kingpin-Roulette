3b5caaf3b13554d5f70754600a53c7e8d20b83f9
Takanu Kyriako <takanu@users.noreply.github.com>
2018-05-31 20:20:54 +0100

    Removed some un-needed details.

--- Sources/App/main.swift
+++ Sources/App/main.swift
@@ -7,11 +7,6 @@ import Pelican
 // Make sure you set up Pelican manually so you can assign it variables.
 let pelican = try PelicanBot()
 
-// A selective whitelist for the beta.
-let chatWhitelist = [-1001073943101, -1001310424502, -1001049787403,     // Properly, Test Group, Saltworld
-	-1001085205161, -1001050908134, -1001144343222, -1001126640483]  // Pantsuit Nation, TestONSGroup, TGP Retarded, Spooky House Test Arena
-
-
 let gameBuilder = SessionBuilder(name: "Game",
                                  spawner: Spawn.perChatID(updateType: [.message, .callbackQuery, .inlineQuery], chatType: [.group, .supergroup]),
                                  idType: .chat,
